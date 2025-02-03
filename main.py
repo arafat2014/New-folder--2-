30a35ae9a99ea0d5270e1b438cc7cf174b2bfbd3
Arafat <arafatsharif7890@gmail.com>
2025-02-03 23:23:34 +0600

    Class Methods in Python

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(66)
+class Employ:
+    company = "Apple "
+    def show(self):
+        print(f"The employ name is {self.name} and the company name is {self.company}")
+        
+    def changCompany(cls, newCompany):
+        cls.company = newCompany
+        
+e1 = Employ()
+e1.name = "Arafat"
+e1.show()
+e1.changCompany("tesla")
+e1.show()
