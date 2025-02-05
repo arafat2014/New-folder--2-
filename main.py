5129f15ed8ab2f96940e9c804fc820feee644596
Arafat <arafatsharif7890@gmail.com>
2025-02-05 23:00:33 +0600

    renaming text file

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-class Employ:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
-    
-    def show(self):
-        print(f"The employ name is {self.name} and his salary is {self.salary}")
-        
-a1 = Employ("Arafat", 12000)
-a1.show()
-    
+import os
+
+os.rename("clutteredFolder/file.txt","clutteredFolder/arafat.tex")
