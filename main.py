9edaf1593ca6c7d2511650ed1cfb505eb4c808ba
Arafat <arafatsharif7890@gmail.com>
2025-02-10 23:55:09 +0600

    __dict__

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ class Person:
         self.name = name
         self.age = age
         
-    
+    def showDetails(self):
+        print(f"The name of student is {self.name} and the age of the student is {self.age} ")
 
 p = Person("Arafat" , 25)
+p.showDetails()
 print(p.__dict__)
