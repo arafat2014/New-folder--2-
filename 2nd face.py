a0f024f09ea5f7d1961f5c853d8bb98344215dd5
Arafat <arafatsharif7890@gmail.com>
2025-02-18 15:48:45 +0600

    Method Overriding in Python

--- 2nd face.py
+++ 2nd face.py
@@ -1,11 +1,21 @@
-class Employ:
-    name = "Arafat"
-    def __len__(self):
-        i = 0
-        for c in self.name:
-            i = i+1
-        return i
+class Shape:
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y
+        
+    def area(self):
+        return self.x * self.y
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        
+    def area(self):
+        return 3.14 * self.radius * self.radius
     
-e = Employ()
-print(e.name)
-print(len(e))
+rec = Shape(3, 5)
+print(rec.area())
+
+c = Circle(5)
+print(c.area())
+    
