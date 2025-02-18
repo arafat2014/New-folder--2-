568b7ee349c789aec43563e9c9803b8e9e725893
Arafat <arafatsharif7890@gmail.com>
2025-02-18 16:11:35 +0600

    Vector

--- 2nd face.py
+++ 2nd face.py
@@ -1,21 +1,11 @@
-class Shape:
-    def __init__(self, x , y):
-        self.x = x
-        self.y = y
-        
-    def area(self):
-        return self.x * self.y
-
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
-        
-    def area(self):
-        return 3.14 * self.radius * self.radius
+class Vector:
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
     
-rec = Shape(3, 5)
-print(rec.area())
-
-c = Circle(5)
-print(c.area())
+    def __str__ (self):
+        return f"{self.i} i + {self.j} j + {self.k} k"
     
+v = Vector(3, 5, 7)
+print(v)
