617ec01dde2662d4c416d7bdd0a7ed86121eb0b4
Arafat <arafatsharif7890@gmail.com>
2025-02-18 22:21:06 +0600

    gfdhg

--- 2nd face.py
+++ 2nd face.py
@@ -1,24 +1,3 @@
-class Employee:
-    def __init__(self, name):
-        self.name = name  
-        
-    def show(self):
-        print(f"The name is {self.name}")      
-class Dancer:
-    def __init__(self, name):
-        self.name = name
-        
-    def show(self):
-        print(f"The Dance is {self.name}")    
-class DancerEmploy(Employee , Dancer):
-    def __init__(self, name, dance):
-        self.name = name
-        self.dance = dance
-        
-o = DancerEmploy("Arafat", "Kathak")
-print(o.name)
-print(o.dance)
-o.show()
-print(DancerEmploy.mro())
+print(23)
 
     
